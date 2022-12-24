f56eb9d92bb2f0d381dc65f37034740be880cdcb
lewisfff <lewisfff>
2022-12-24 19:55:44 +0000

    Typo!

--- database/migrations/2022_12_03_200825_create_reminders_table.php
+++ database/migrations/2022_12_03_200825_create_reminders_table.php
@@ -20,8 +20,8 @@ return new class extends Migration
             $table->timestamps();
             $table->dateTime('date')->nullable();
             $table->string('message');
-            $table->foreignId('channel_id')->constrained();
-            $table->foreignId('message_id')->constrained();
+            $table->foreignId('channel_id');
+            $table->foreignId('message_id');
             $table->foreignId('user_id')->constrained();
         });
     }
