29ac366ffa70c410a33af5d9160977690122ea7c
lewisfff <lewisfff>
2022-12-27 11:48:44 +0000

    Refactor reminder service to allow multiple date indicators

--- app/Services/ReminderService.php
+++ app/Services/ReminderService.php
@@ -15,39 +15,38 @@ class ReminderService
      */
     public static function parseCommand(string $command): array
     {
-        $initial = $command;
-
         $command = strtolower($command);
         $command = str_replace('remind me to ', '', $command);
 
         $message = '';
         $date = null;
-
-        // array of words that can be used to indicate a date
         $dateIndicators = [' on ', ' at ', ' in ', ' next ', ' this '];
+        $usedIndicator = null;
         $dateString = null;
+        $pos = false;
 
-        // if any of the $dateIndicators are found, find the last occurrence of one of them and then split the command into two parts, the message and the date
-        // explode cannot be used as it will split the command into multiple parts if multiple $dateIndicators are found
-        // also strip out the $dateIndicator string from the date part
-        // if no $dateIndicators are found, the message is the entire command and the date is null
+        // find the last occurrence of a $dateIndicator
         foreach ($dateIndicators as $dateIndicator) {
-            $pos = strrpos($command, $dateIndicator);
-            if ($pos !== false) {
-                $message = substr($command, 0, $pos);
-                $dateString = trim(substr($command, $pos + strlen($dateIndicator)));
-                break;
+            // if pos is greater than the current pos, set it as the new pos
+            if (strrpos($command, $dateIndicator) > $pos) {
+                $pos = strrpos($command, $dateIndicator);
+                $usedIndicator = $dateIndicator;
             }
         }
 
-        // if no $dateIndicator was found, assume the last word is the date for example "remind me to test tomorrow"
-        if ($dateString === null) {
-            $message = substr($command, 0, strrpos($command, ' '));
-            $dateString = trim(substr($command, strrpos($command, ' ')));
+        if ($pos !== false) {
+            $message = substr($command, 0, $pos);
+            $dateString = trim(substr($command, $pos + strlen($usedIndicator)));
         }
 
-        // can a Carbon date be made from $dateString
-        // put this in a try/catch block
+        // if no $usedIndicator was found, assume the last word is the date for example "remind me to test tomorrow"
+        if ($dateString === null) {
+            $lastSpacePos = strrpos($command, ' ');
+            $message = substr($command, 0, $lastSpacePos);
+            $dateString = trim(substr($command, $lastSpacePos));
+        }
+
+        // try to create a Carbon date from $dateString
         try {
             $date = \Carbon\Carbon::parse($dateString);
 
